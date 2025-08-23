f9cc2854dc1197161cb537f71b09ce31cd9d26e9
пк <negulyaevd65@gmail.com>
2025-08-23 23:32:16 +0400

    test

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject Platform;
+    Vector3 finishsize = new Vector3();
+    public float currentsize;
+
+    /*
+    IEnumerable reduce()
+    {
+        while (currentsize != finishsize)
+        {
+            Platform.transform.localScale = ;
+
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+    */
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
