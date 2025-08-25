f6922ead363475433a47ca557ef3a61569a39bc8
Kirill_Senior <kirill.keynch@gmail.com>
2025-08-25 20:19:46 +0400

    InputManager changed

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,8 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
+    public static InputManager Instance { get; private set; }
 
+    private InputActions inputActions;
+
+    private void Awake()
+    {
+        Instance = this;
+        inputActions = new InputActions();
+        inputActions.Enable();
+    }
+
+    public Vector2 GetMovementVector()
+    {
+        Vector2 inputVector = inputActions.BallFight.Move.ReadValue<Vector2>();
+        return inputVector;
+    }
 }
